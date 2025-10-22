123e2cd2a977d5fb9b74a2fb4e7ea493a5690677
thaovyvy85 <23521836@gm.uit.edu.vn>
2025-10-22 23:16:48 +0700

    Change the useRouteElements

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppProvider } from './contexts/app.context'
+import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    }
+  }
+})
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <App />
+        </AppProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
+)
--- Frontend/src/useRouteElements.tsx
+++ Frontend/src/useRouteElements.tsx
@@ -2,32 +2,38 @@ import { useRoutes } from "react-router-dom";
 import path from "././constants/path";
 
 // --- Public Pages ---
-import ExplorePage from "./pages/Explore/Explore";
+// import ExplorePage from "./pages/Explore/Explore";
 import EventDetailsPage from "./pages/EventDetail/EventDetail";
-import TicketsPage from "./pages/Tickets/Tickets";
-import ProfilePage from "./pages/Profile/Profile";
-import LoginPage from "./pages/Login";
+// import TicketsPage from "./pages/Tickets/Tickets";
+// import ProfilePage from "./pages/Profile/Profile";
+import LoginPage from "./pages/Login/Login";
 import SigninPage from "./pages/Signin/Signin";
 
 // --- Organizer Pages ---
-import OrganizerDashboard from "./pages/organizer/dashboard";
+// import OrganizerDashboard from "./pages/organizer/dashboard";
 import CreateEventPage from "./pages/organizer/create-event";
-import ManageEventPage from "./pages/organizer/[eventId]";
-import QRScannerPage from "./pages/organizer/scanner";
+// import ManageEventPage from "./pages/organizer/[eventId]";
+// import QRScannerPage from "./pages/organizer/scanner";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 import RejectedRoute from "./routes/RejectedRoute";
+import MainLayout from "./layouts/MainLayout";
+import RegisterLayout from "./layouts/RegisterLayout";
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     // Public
-    {
-      path: path.home,      // "/"
-      element: <ExplorePage />,
-    },
+    // {
+    //   path: path.home,      // "/"
+    //   element: <ExplorePage />,
+    // },
     {
       path: path.event_details, // "/events/:id"
-      element: <EventDetailsPage />,
+      element: (
+      <MainLayout>
+      <EventDetailsPage />
+      </MainLayout>
+      ),
     },
 
     // Authenticated-only
@@ -35,14 +41,19 @@ export default function useRouteElements() {
       path: "/",
       element: <ProtectedRoute />,
       children: [
-        { path: path.tickets, element: <TicketsPage /> },        // "/tickets"
-        { path: path.profile, element: <ProfilePage /> },        // "/profile"
+        // { path: path.tickets, element: <TicketsPage /> },        // "/tickets"
+        // { path: path.profile, element: <ProfilePage /> },        // "/profile"
 
         // Organizer
-        { path: path.organizer_dashboard, element: <OrganizerDashboard /> }, // "/organizer"
-        { path: path.organizer_create_event, element: <CreateEventPage /> }, // "/organizer/create-event"
-        { path: path.organizer_manage_event, element: <ManageEventPage /> }, // "/organizer/events/:eventId"
-        { path: path.organizer_scanner, element: <QRScannerPage /> },        // "/organizer/scanner"
+        // { path: path.organizer_dashboard, element: <OrganizerDashboard /> }, // "/organizer"
+        { path: path.organizer_create_event, 
+          element: (
+            <MainLayout>
+          <CreateEventPage />
+            </MainLayout>
+          ) }, // "/organizer/create-event"
+        // { path: path.organizer_manage_event, element: <ManageEventPage /> }, // "/organizer/events/:eventId"
+        // { path: path.organizer_scanner, element: <QRScannerPage /> },        // "/organizer/scanner"
       ],
     },
 
@@ -51,8 +62,21 @@ export default function useRouteElements() {
       path: "/",
       element: <RejectedRoute />,
       children: [
-        { path: path.login, element: <LoginPage /> },   // "/login"
-        { path: path.signup, element: <SigninPage /> }, // "/signup"
+        { 
+          path: path.login, 
+          element: (
+          <RegisterLayout>
+            <LoginPage />
+          </RegisterLayout> 
+          )
+        },   // "/login"
+        { path: path.signup, 
+          element: (
+          <RegisterLayout>
+            <SigninPage />
+          </RegisterLayout>
+          ) 
+        }, // "/signup"
       ],
     },
   ]);
